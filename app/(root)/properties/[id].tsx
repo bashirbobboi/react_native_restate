8e6de3a45909d7703184ef67e9acaf3df99a96e5
bashirbobboi <125030027+bashirbobboi@users.noreply.github.com>
2025-10-24 10:01:18 +0100

    Enhance property details view by adding agent information and contact options, including email and phone links. Adjust layout for improved presentation.

--- app/(root)/properties/[id].tsx
+++ app/(root)/properties/[id].tsx
@@ -1,10 +1,11 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { View, Text, Image, TouchableOpacity, Linking } from 'react-native'
 import React from 'react'
 import { router, useLocalSearchParams } from 'expo-router'
 import { useAppwrite } from '@/lib/useAppwrite'
 import { getProperty } from '@/lib/appwrite'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import icons from '@/constants/icons'
+import images from '@/constants/images'
 
 const Property = () => {
     const { id } = useLocalSearchParams();
@@ -26,7 +27,7 @@ const Property = () => {
                 </TouchableOpacity>
                 <Image source={icons.bell} className="w-6 h-6" />
       </View>
-      <View className='pl-6 mt-5'>
+      <View className='pl-6 pr-6 mt-5'>
         <Text className='text-3xl font-rubik-bold'>
           Modernica Apartment
         </Text>
@@ -57,10 +58,28 @@ const Property = () => {
             <Text className='font-semibold py-3'> 2000 sqft </Text>
           </View>
         </View>
-        <View className="h-px bg-gray-200 my-2" />
-        <View className='mt-6'>
+        <View className="h-px bg-primary-200 my-10" />
+        <View>
         <Text className='text-2xl font-rubik-bold'>Agent</Text>
-      </View>
+        <View className='flex flex-row mt-3 items-center justify-between'>
+          <View className='flex flex-row items-center'>
+            <Image source={images.avatar} className='w-20 h-20' />
+            <View className='pl-4'>
+              <Text className='text-xl font-rubik-semibold text-black-300'>Natasya Wilodra</Text>
+              <Text className='font-rubik-medium text-black-200 mt-1'>Owner</Text>
+            </View>
+          </View>
+
+          <View className='flex flex-row gap-5'>
+            <TouchableOpacity onPress={() => Linking.openURL("mailto:johndoe@gmail.com")}>
+              <Image source={icons.chat} className='w-8 h-8' />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => Linking.openURL("tel:+1234567890")}>
+              <Image source={icons.phone} className='w-8 h-8' />
+            </TouchableOpacity>
+          </View>
+        </View>
+        </View>
       </View>
     </SafeAreaView>
   )
