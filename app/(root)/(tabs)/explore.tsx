8736a97c72ca3adfc2ed85fc95586e435fb3ef79
bashirbobboi <125030027+bashirbobboi@users.noreply.github.com>
2025-10-24 02:26:23 +0100

    Add header component to Explore tab with back navigation and search prompt

--- app/(root)/(tabs)/explore.tsx
+++ app/(root)/(tabs)/explore.tsx
@@ -58,6 +58,13 @@ export default function Explore() {
           }
           ListHeaderComponent={ 
             <View className="px-5">
+              <View className="flex flex-row items-center justify-between mt-5">
+                <TouchableOpacity onPress={() => router.back()} className="flex flex-row bg-primary-200 rounded-full size-11 items-center justify-center">
+                  <Image source={icons.backArrow} className="size-5" />
+                </TouchableOpacity>
+                <Text className="text-base mr-2 text-center font-rubik-medium text-black-300">Search for Your Ideal Home</Text>
+                <Image source={icons.bell} className="w-6 h-6" />
+              </View>
               <Search />
 
               <View className="mt-5">
